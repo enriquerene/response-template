6f9e43c96e76cad0406d70622a7f78208ee4f4d0
Enrique René <enriquerenebr@gmail.com>
2021-03-24 21:15:16 -0300

    automated tests for software quality and behavior mapping

--- src/ResponseTemplate.php
+++ src/ResponseTemplate.php
@@ -3,78 +3,175 @@
 namespace RESTfulTemplate;
 
 use \Exception;
-$statusMapFromFile = require "./StatusMap.php";
 
 class ResponseTemplate
 {
-	const STATUS_MAP = $statusMapFromFile;
-	const SELF_LINK_EXCEPTION_MESSAGE = "A propriedade \"self\" deve possuir um URL definida em links.";
-	const STATUS_CODE_EXCEPTION_MESSAGE = "O código de estado HTTP deve ser um entre os seguintes: [" . implode( ", ", array_keys( $this->STATUS_MAP ) ) . "].";
+    /**
+     * @see https://developer.mozilla.org/docs/Web/HTTP/Status
+     */
+    const STATUS_MAP = [
+        /* informational responses */
+        100 => "Continue",
+        101 => "Switching Protocol",
+        102 => "Processing",
+        103 => "Early Hints",
+        /* successful responses */
+        200 => "Ok",
+        201 => "Created",
+        202 => "Accepted",
+        203 => "Non-Authoritative Information",
+        204 => "No Content",
+        205 => "Reset Content",
+        206 => "Partial Content",
+        207 => "Multi-Status",
+        208 => "Multi-Status",
+        226 => "IM Used",
+        /* redirects */
+        300 => "Multiple Choice",
+        301 => "Moved Permanently",
+        302 => "Found",
+        303 => "See Other",
+        304 => "Not Modified",
+        305 => "Use Proxy",
+        306 => "unused ",
+        307 => "Temporary Redirect",
+        308 => "Permanent Redirect",
+        /* client errors */
+        400 => "Bad Request",
+        401 => "Unauthorized",
+        402 => "Payment Required",
+        403 => "Forbidden",
+        404 => "Not Found",
+        405 => "Method Not Allowed",
+        406 => "Not Acceptable",
+        407 => "Proxy Authentication Required",
+        408 => "Request Timeout",
+        409 => "Conflict",
+        410 => "Gone",
+        411 => "Length Required",
+        412 => "Precondition Failed",
+        413 => "Payload Too Large",
+        414 => "URI Too Long",
+        415 => "Unsupported Media Type",
+        416 => "Requested Range Not Satisfiable",
+        417 => "Expectation Failed",
+        418 => "I'm a teapot",
+        421 => "Misdirected Request",
+        422 => "Unprocessable Entity",
+        423 => "Locked",
+        424 => "Failed Dependency",
+        425 => "Too Early",
+        426 => "Upgrade Required",
+        428 => "Precondition Required",
+        429 => "Too Many Requests",
+        431 => "Request Header Fields Too Large",
+        451 => "Unavailable For Legal Reasons",
+        /* server errors */
+        500 => "Internal Server Error",
+        501 => "Not Implemented",
+        502 => "Bad Gateway",
+        503 => "Service Unavailable",
+        504 => "Gateway Timeout",
+        505 => "HTTP Version Not Supported",
+        506 => "Variant Also Negotiates",
+        507 => "Insufficient Storage",
+        508 => "Loop Detected",
+        510 => "Not Extended",
+        511 => "Network Authentication Required"
+    ];
+	const SELF_LINK_EXCEPTION_MESSAGE = 'A propriedade "self" deve possuir um URL definida em links.';
+	const STATUS_CODE_EXCEPTION_MESSAGE = 'O código de estado HTTP deve ser um entre os seguintes: 100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511.';
+    const ARG_TYPE_EXCEPTION_MESSAGE = 'Given argument missmatches with required type.';
 
 	/**
 	 * @var int
-	 *
 	 */
-    private $statusCode;
+	private $statusCode;
 
 	/**
 	 * @var array
-	 *
 	 */
 	private $links = [];
 
+	/**
+	 * @var string
+	 */
+	private $basename = '';
 
 	function __construct ( int $statusCode )
 	{
-		if ( ! in_array( $statusCode, array_keys( $this->STATUS_MAP ), true ) )
-			throw new Exception( self::STATUS_CODE_EXCEPTION_MESSAGE );
-
+		if ( ! in_array( $statusCode, array_keys( self::STATUS_MAP ), true ) ) throw new Exception( self::STATUS_CODE_EXCEPTION_MESSAGE );
 		$this->statusCode = $statusCode;
-		$url = $this->getBasename();
-
+		$this->basename = $this->getBasename();
+        $selfUrl = $this->basename . $this->getRequestPath();
+        $selfQueryString = $this->getRequestQueryString();
+        if (!empty($selfQueryString)) {
+            $selfUrl .= '?' . $this->getRequestQueryString();
+        }
+        $this->setLink('self', $selfUrl, $this->getRequestMethod());
 	}
 
     /**
-     * @param array $data Requested data
-	 *
+     * Builds an associative array based on ResponseTemplate configuration, status code and given data and links.
+     *
+     * @param Type $data Data to be attached to data property of response template.
+     * @return Description
      */
-	public function build ( $data = null ): array
+	public function build ( array $data = [] ): array
 	{
+        if (!is_array($data)) throw new Exception(self::ARG_TYPE_EXCEPTION_MESSAGE); 
 		return [
 			"status" => $this->getStatus(),
-			"data" => $data,
+			"data" => (count($data) > 0 ? $data: null),
 			"links" => $this->links
 		];
     }
 
+    /**
+     * Get status structure as array. Status is an structure containing "code" and "message" fields.
+     *
+     * @return Status associative array.
+     */
 	public function getStatus (): array
 	{
         return [
 			"code" => $this->statusCode,
-			"message" => $this->getStatusMessage()
+			"message" => $this->getStatusMessage().'.'
 		];
     }
 
-	public function getStatusMessage ()
+    /**
+     * Get HTTP status message accordingly given status code.
+     *
+     * @return HTTP Message associated to given status code.
+     */
+	public function getStatusMessage (): string
 	{
-        return $this->STATUS_MAP[ $this->statusCode ];
+        return self::STATUS_MAP[ $this->statusCode ];
     }
 
-	public function setLink( string $key, $value )
+    /**
+     * Set a Link to response template.
+     *
+     * @param string $key Link key name
+     * @param string $url Link url
+     * @param string $method Link method
+     * @return Instance of ResponseTemplate.
+     */
+	public function setLink( string $key, string $url, string $method = 'GET' ): ResponseTemplate
 	{
-		$this->links[ $key ] = $value;
+        $this->links[$key] = [
+            'url' => $url,
+            'method' => $method
+        ];
 		return $this;
 	}
 
-	public function setLinks( array $links ): ResponseTemplate
-	{
-		foreach ( $links as $key => $value )
-		{
-			$this->setLink( $key, $value );
-		}
-		return $this;
-	}
-
+    /**
+     * Get basename from server. Protocol is included.
+     *
+     * @return Basename string including protocol.
+     */
 	public function getBasename (): string
 	{
 		$protocol = ( empty( $_SERVER[ "HTTPS" ] ) ? "http" : "https" );
@@ -82,11 +179,42 @@ class ResponseTemplate
 		return "$protocol://$host";
 	}
 
-	public function getStatusMap (): array
+    /**
+     * Get HTTP request query string.
+     *
+     * @return HTTP request query string.
+     */
+	public function getRequestQueryString(): string
 	{
-		return $this->STATUS_MAP;
-	}
+        return (empty($_SERVER['QUERY_STRING'])? '':$_SERVER['QUERY_STRING']);
+    }
 
+    /**
+     * Get HTTP request method.
+     *
+     * @return HTTP request method.
+     */
+	public function getRequestMethod(): string
+	{
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    /**
+     * Get HTTP request path.
+     *
+     * @return HTTP request path.
+     */
+	public function getRequestPath(): string
+	{
+        return $_SERVER['PATH_INFO'];
+    }
+
+    /**
+     * Checks if a key link is defined in response template.
+     *
+     * @param string $key The key of links in response template.
+     * @return True if the key is defined, false otherwise.
+     */
 	public function isLinkDefined ( string $key ): bool
 	{
 		return ! empty( $this->links[ $key ] );
---
+++ tests/ResponseTemplateBasicTest.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace RESTfulTemplate;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+//use RESTfulTemplate\ResponseTemplate;
+require_once __DIR__ . '/../src/ResponseTemplate.php';
+
+$_SERVER['HTTP_HOST'] = 'myserver';
+$_SERVER['REQUEST_METHOD'] = 'GET';
+$_SERVER['PATH_INFO'] = '/';
+$_SERVER['QUERY_STRING'] = null;
+final class ResponseTemplateBasicTest extends TestCase
+{
+    /**
+     * ResponseTemplate requires an valid HTTP Status code.
+     */
+	public function testValidHTTPStatusCodeIsRequired(): void
+	{
+        $message = '';
+        try {
+            $rest = new ResponseTemplate(1);
+        } catch(\Exception $e) {
+            $message = $e->getMessage();
+        }
+        $this->assertSame(ResponseTemplate::STATUS_CODE_EXCEPTION_MESSAGE, $message);
+    }
+
+    /**
+     * Build method without arguments must return null data property, status property including code and message, and self key inside links property.
+     */
+	public function testBuildDataNullStatusOk(): void
+	{
+        $expectedResponse = [
+            'status' => [
+                'code' => 200,
+                'message' => 'Ok.'
+            ],
+            'data' => null,
+            'links' => [
+                'self' => [
+                    'url' => 'http://' . $_SERVER['HTTP_HOST'].$_SERVER['PATH_INFO'],
+                    'method' => $_SERVER['REQUEST_METHOD']
+                ]
+            ]
+        ];
+        $rest = new ResponseTemplate(200);
+        $actualResponse = $rest->build();
+        $this->assertSame($expectedResponse, $actualResponse);
+    }
+
+    /**
+     * Build method accepts array as data property.
+     */
+	public function testBuildDataArrayStatusOk(): void
+	{
+        $data = [['foo' => 'bar'], ['foo' => 'baz']];
+        $expectedResponse = [
+            'status' => [
+                'code' => 200,
+                'message' => 'Ok.'
+            ],
+            'data' => $data,
+            'links' => [
+                'self' => [
+                    'url' => 'http://' . $_SERVER['HTTP_HOST'].$_SERVER['PATH_INFO'],
+                    'method' => $_SERVER['REQUEST_METHOD']
+                ]
+            ]
+        ];
+        $rest = new ResponseTemplate(200);
+        $actualResponse = $rest->build($data);
+        $this->assertSame($expectedResponse, $actualResponse);
+    }
+
+    /**
+     * It can set link property custom key append only query string (functionality inspired from pagination in links).
+     */
+	public function testSetLinkCustomKey(): void
+	{
+        $expectedResponse = [
+            'status' => [
+                'code' => 200,
+                'message' => 'Ok.'
+            ],
+            'data' => null,
+            'links' => [
+                'self' => [
+                    'url' => 'http://' . $_SERVER['HTTP_HOST'].$_SERVER['PATH_INFO'],
+                    'method' => $_SERVER['REQUEST_METHOD']
+                ],
+                'next' => [
+                    'url' => 'http://' . $_SERVER['HTTP_HOST'].$_SERVER['PATH_INFO'] . '?page=2',
+                    'method' => 'GET'
+                ]
+            ]
+        ];
+        $rest = new ResponseTemplate(200);
+        $rest = $rest->setLink('next', 'http://' . $_SERVER['HTTP_HOST'].$_SERVER['PATH_INFO'] . '?page=2', 'GET');
+        $actualResponse = $rest->build();
+        $this->assertSame($expectedResponse, $actualResponse);
+    }
+
+    /**
+     * It can verify if a link key is already defined.
+     */
+	public function testLinkKeyIsDefined(): void
+	{
+        $expected = true;
+        $rest = new ResponseTemplate(200);
+        $rest = $rest->setLink('next', 'http://' . $_SERVER['HTTP_HOST'].$_SERVER['PATH_INFO'] . '?page=2', 'GET');
+        $actual = $rest->isLinkDefined('next');
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * It can verify if a link key is not defined yet.
+     */
+	public function testLinkKeyIsNotDefined(): void
+	{
+        $expected = false;
+        $rest = new ResponseTemplate(200);
+        $actual = $rest->isLinkDefined('next');
+        $this->assertSame($expected, $actual);
+    }
+}
